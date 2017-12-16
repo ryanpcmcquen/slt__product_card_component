d489837f9391cae7f07a40349e743d7c839eaf33
Ryan P.C. McQuen <ryanpcmcquen@member.fsf.org>
2017-12-15 17:27:16 -0800

    More CSS tidying.

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,10 +1,14 @@
 import React from "react";
 
+const activeCartStyle = {
+	color: "#E57D24"
+};
+
 export default class Cart extends React.Component {
 	render() {
 		return (
 			<div>
-				<i className="glyphicon glyphicon-shopping-cart" />
+				<i className="glyphicon glyphicon-shopping-cart" style={activeCartStyle} />
 			</div>
 		);
 	}
