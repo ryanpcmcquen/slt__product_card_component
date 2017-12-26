6ae61c524460274767b5c44416b95431a088d890
Ryan P.C. McQuen <ryanpcmcquen@member.fsf.org>
2017-12-26 10:40:38 -0800

    Add more comments.

--- src/reducers/cartReducer.js
+++ src/reducers/cartReducer.js
@@ -1,3 +1,12 @@
+// There is a bit of dark magic here.
+// - When calling `store.dispatch`, the object
+//   that is passed applies to `action` in this function,
+//   not `state`.
+// - The default parameter here creates an object with a
+//   `cart` array as a property, if it does not exist.
+// - The spread operators inside of the `return` make
+//   sure that all former keys & values are passed to the new
+//   object that is returned.
 const cartReducer = (state = { cart: [] }, action) => {
 	switch (action.type) {
 		case "ADD_TO_CART":
--- src/reducers/sltReducers.js
+++ src/reducers/sltReducers.js
@@ -1,6 +1,7 @@
 import * as Redux from "redux";
 import cartReducer from "./cartReducer";
 
+// This combines all reducers, so they can be modular:
 const sltReducers = Redux.combineReducers({
 	cartReducer
 });
