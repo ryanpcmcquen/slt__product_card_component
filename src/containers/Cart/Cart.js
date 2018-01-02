3cbd84665cec8656a31d3ee779bdc9d127a285e8
Ryan P.C. McQuen <ryanpcmcquen@member.fsf.org>
2018-01-02 15:00:06 -0800

    Grab store from context object in containers.

--- src/containers/Cart/Cart.js
+++ src/containers/Cart/Cart.js
@@ -1,15 +1,14 @@
 import React from "react";
-import sltStore from "../../store/sltStore";
+import PropTypes from "prop-types";
 
 const activeCartStyle = {
 	color: "#E57D24"
 };
 
-// Because this component only has a `render` and
-// does not require `this.props`, it can be a function,
-// rather than a class.
-const Cart = () => {
-	let aCart = sltStore.getState().cartReducer.cart || [];
+// Because this component only has a `render` it
+// can be a function, rather than a class.
+const Cart = (props, context) => {
+	let aCart = context.store.getState().cartReducer.cart || [];
 	return (
 		<div>
 			<i
@@ -19,6 +18,10 @@ const Cart = () => {
 			<span className="shopping-cart-count">({aCart.length || 0})</span>
 		</div>
 	);
+};
+// This links the store to the context object.
+Cart.contextTypes = {
+	store: PropTypes
 };
 
 export default Cart;
--- src/containers/ProductCard/ProductCard.js
+++ src/containers/ProductCard/ProductCard.js
@@ -1,15 +1,15 @@
 import React from "react";
 import Card from "../../components/Card/Card";
 import Button from "../../components/Button/Button";
-import sltStore from "../../store/sltStore";
+import PropTypes from "prop-types";
 import { addedToCart } from "../../actions/cartActions.js";
 
-const ProductCard = props => {
+const ProductCard = (props, context) => {
 	let { addToCart, price } = props;
 	// Determine 'on-the-fly' if the sku is inside
 	// of our cart object. This keeps us from having
 	// a needless boolean and wasting memory.
-	let isInCart = sltStore
+	let isInCart = context.store
 		.getState()
 		.cartReducer.cart.find(item => item.hasOwnProperty(props.sku));
 	let badge = isInCart ? "ADDED TO CART" : null;
@@ -27,6 +27,10 @@ const ProductCard = props => {
 			)}
 		</div>
 	);
+};
+// This links the store to the context object.
+ProductCard.contextTypes = {
+	store: PropTypes
 };
 
 export default ProductCard;
